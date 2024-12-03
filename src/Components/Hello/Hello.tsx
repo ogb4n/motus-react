0ab133886f9a849be20dc4dc7394467740afdf83
ogb4n <ogb4n@protonmail.com>
2024-12-03 12:16:57 +0100

    install & config tailwind

--- src/Components/Hello/Hello.tsx
+++ src/Components/Hello/Hello.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export const Hello = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1 className="text-blue-700">Hello World!</h1>
     </div>
   );
 };
